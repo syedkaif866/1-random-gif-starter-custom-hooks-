d87123060b2912ce64770fc010a5daf8b02d3348
syedkaif866 <syedkaif866@gmail.com>
2024-09-17 16:55:35 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+// hook -> it is a utility function .it is used to do a particular work
+//custom hook -> can also be created as a utility function
+
+//npm i axios -> it is used to call api
